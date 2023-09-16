9cfb6ef30d49486023a2b7d7dd161c5748a4d36e
shahabas ahammed <shahabasvellakkatt@gmail.com>
2023-09-16 20:43:47 +0530

    delete messege.html include file

--- accounts/admin_views.py
+++ accounts/admin_views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
-from django.views.decorators.csrf import csrf_exempt
 from .models import *
 from django.contrib import messages
 from django.http import HttpResponse,JsonResponse
--- turf/urls.py
+++ turf/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path("",include('accounts.urls')),
     path('client/', include('accounts.urls')),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
